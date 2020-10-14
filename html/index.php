88881a0d63a819e062d2c09aa8a1b652f24d3ac3
lorismrcr <61010042+lorismrcr@users.noreply.github.com>
2020-10-14 23:55:52 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -2,8 +2,8 @@
 echo "Une petite fille va voir son père en pleurant : 
 - Papa ! Tout le monde se moque de moi à l’école parce que j’ai une grande bouche !
 - Ne les écoute pas, ils disent n’importe quoi. Allez, prends ta pelle et bois ta soupe !";
-echo "<img src='{$data[https://image-uniservice.linternaute.com/image/450/6188869355/1168394.jpg]}'/>";
-echo "<img src='{$data[https://image-uniservice.linternaute.com/image/450/6188869355/1168394.jpg]}'/>";
-echo "<img src='{$data[https://image-uniservice.linternaute.com/image/450/6188869355/1168394.jpg]}'/>";
+
+$image = "ttps://image-uniservice.linternaute.com/image/450/6188869355/1168394"."jpg";
+print '<img src="$image" alt="texte alternatif" />';
 echo "Baaaa CPE c'est chimie quoi, c'est moyen";
 ?>
