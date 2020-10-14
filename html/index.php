852455e9b7c973116697c332e59f75787afc3422
jsbubu <61010167+jsbubu@users.noreply.github.com>
2020-10-14 23:23:14 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,5 +1,9 @@
 <?php
 echo "Une petite fille va voir son père en pleurant : 
 - Papa ! Tout le monde se moque de moi à l’école parce que j’ai une grande bouche !
-- Ne les écoute pas, ils disent n’importe quoi. Allez, prends ta pelle et bois ta soupe !"
+- Ne les écoute pas, ils disent n’importe quoi. Allez, prends ta pelle et bois ta soupe !";
+echo "<img src='{$data[https://image-uniservice.linternaute.com/image/450/6188869355/1168394.jpg]}'/>";
+echo "<img src='{$data[https://image-uniservice.linternaute.com/image/450/6188869355/1168394.jpg]}'/>";
+echo "<img src='{$data[https://image-uniservice.linternaute.com/image/450/6188869355/1168394.jpg]}'/>";
+echo "Baaaa CPE c'est chimie quoi, c'est moyen";
 ?>
