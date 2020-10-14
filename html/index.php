27f6c983aa794b99e119374c2c61214b8a88e114
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-10-14 22:31:44 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Une petite fille va voir son père en pleurant : <br/>
-- Papa ! Tout le monde se moque de moi à l’école parce que j’ai une grande bouche !<br/>
+echo "Une petite fille va voir son père en pleurant : 
+- Papa ! Tout le monde se moque de moi à l’école parce que j’ai une grande bouche !
 - Ne les écoute pas, ils disent n’importe quoi. Allez, prends ta pelle et bois ta soupe !"
 ?>
