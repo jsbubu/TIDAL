4d19c7561b7c65fc3f20b97aff03da2cbb7fdc46
lorismrcr <61010042+lorismrcr@users.noreply.github.com>
2020-10-19 23:17:47 +0200

    Update test.php

--- html/controllers/test.php
+++ html/controllers/test.php
@@ -1,10 +1,7 @@
 <?php
 
-require_once(_CORE_.'products.php');
-$prod = new products();
-
 //On appelle la fonction getAll()
-$allProducts = $prod->getAll();
+$allProducts = products::getAll();
 //On transmets les variables à Smarty
 $smarty->assign('products', $allProducts);
 ?>
