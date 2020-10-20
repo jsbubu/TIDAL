2dcec86995d2e2cfef833ca2ad7887184a11549f
lorismrcr <61010042+lorismrcr@users.noreply.github.com>
2020-10-20 15:39:30 +0200

    Update createProduct.php

--- html/controllers/createProduct.php
+++ html/controllers/createProduct.php
@@ -1,18 +1,3 @@
 <?php
-require_once(_ENT_.'product');
 
-if(isset($_POST['send'])){
-    //Récupération du nom de l'image
-    $image_name = $_FILES['image']['name'];
-    //Récupération de la description de l'image
-    $image_desc = htmlspecialchars($_POST['image_desc']);
-    //Récupération de la description du produit
-    $description = htmlspecialchars($_POST['description']);
-    //Récupération du Nom
-    $nom = htmlspecialchars($_POST['nom']);
-    //Récupération du Prix
-    $price = $_POST['price'];
-    //$prod = new product($nom,$description,$price,$image_name,$image_desc);
-    products::addOneProduct($nom,$description,$price,$image_name,$image_desc);
-}
 ?>
