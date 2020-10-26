db8d20d94ae37cbe514310af541c1cd681fd2ff4
lorismrcr <loris.mercier@cpe.fr>
2020-10-26 20:50:29 +0100

    Test

--- html/controllers/test2.php
+++ html/controllers/test2.php
@@ -6,7 +6,7 @@ $allProducts = products::getAll();
 foreach ($allProducts as $image){
    // $temp = base64_encode($image.image);
    // $allProducts[$image.image] = $temp;
-   echo base64_encode(base64_encode($image.image));
+   echo '<img src="data:image/jpg;base64,'.base64_encode(base64_encode($image.image)).'" />';
 }
 //On transmets les variables à Smarty
 $smarty->assign('products', $allProducts);
