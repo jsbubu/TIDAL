78bdb99a8944387d407c3c80442ac2ec869d6300
lorismrcr <loris.mercier@cpe.fr>
2020-10-26 20:33:39 +0100

    Affichage

--- html/controllers/test2.php
+++ html/controllers/test2.php
@@ -3,11 +3,11 @@ $tabImage = array();
 //On appelle la fonction getAll()
 $allProducts = products::getAll();
 
-//foreach ($allProducts as $image){
+foreach ($allProducts as $image){
    // $temp = base64_encode($image.image);
    // $allProducts[$image.image] = $temp;
-//}
-header("Content-Type: image/jpg");
+   echo '<img src="data:image/jpeg;base64,'.base64_encode($image.image).'" />';
+}
 //On transmets les variables à Smarty
 $smarty->assign('products', $allProducts);
 ?>
