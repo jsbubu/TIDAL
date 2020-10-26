deb91891f94d350dced7d152d7ac1933246116f7
lorismrcr <loris.mercier@cpe.fr>
2020-10-26 19:28:12 +0100

    test affichage

--- html/controllers/test2.php
+++ html/controllers/test2.php
@@ -1,7 +1,12 @@
 <?php
-
+$tabImage = array();
 //On appelle la fonction getAll()
 $allProducts = products::getAll();
+
+foreach ($allProducts as $image){
+    $temp = base64_encode($image.image);
+    $allProducts[$image.image] = $temp;
+}
 //On transmets les variables à Smarty
 $smarty->assign('products', $allProducts);
 ?>
--- html/tpl/pages/test2.tpl
+++ html/tpl/pages/test2.tpl
@@ -1,7 +1,6 @@
-
 {foreach from=$products item=product}   
 
+        {$product.name}
         {$product.image}
     <br/>
 {/foreach}
-
