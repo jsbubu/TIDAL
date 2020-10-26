ecf1831aae69c4a9b2f7d82dc5c8402f8a7c8572
lorismrcr <61010042+lorismrcr@users.noreply.github.com>
2020-10-26 12:26:39 +0100

    Update test.php

--- html/controllers/test.php
+++ html/controllers/test.php
@@ -3,6 +3,5 @@
 //On appelle la fonction getAll()
 $allProducts = products::getAll();
 //On transmets les variables à Smarty
-header("Content-type: image/gif");
 $smarty->assign('products', $allProducts);
 ?>
