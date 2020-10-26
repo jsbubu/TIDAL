cd27c0b4f66bf9ee4a55a430ea25b75e03974748
lorismrcr <loris.mercier@cpe.fr>
2020-10-26 23:50:53 +0100

    ajout log bdd

--- html/core/products.php
+++ html/core/products.php
@@ -21,7 +21,7 @@ class products
     //Ajout d'un produit dans la base de donnée
     public function addOneProduct($nom,$description,$filtre,$price,$base64,$img_type,$image_desc){
         global $bdd;
-        $bdd->query("INSERT INTO Productls (name,description,filtre,price,image,img_type,img_description) VALUES ('$nom','$description','$filtre','$price','$base64','$img_type','$image_desc')");
+        $bdd->query("INSERT INTO Products (name,description,filtre,price,image,img_type,img_description) VALUES ('$nom','$description','$filtre','$price','$base64','$img_type','$image_desc')")or die(print_r($bdd->errorInfo()));
         if($bdd){
             echo "<script>alert(\"Ajout du nouveau produit réussi!\")</script>";
         }else{
