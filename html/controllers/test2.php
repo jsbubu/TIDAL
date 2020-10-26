54d6e7c04eb34b58664050776725ef2dc486d870
lorismrcr <loris.mercier@cpe.fr>
2020-10-26 21:17:17 +0100

    Test affichage image

--- html/controllers/test2.php
+++ html/controllers/test2.php
@@ -6,6 +6,7 @@ $allProducts = products::getAll();
 foreach ($allProducts as $image){
    // $temp = base64_encode($image.image);
    // $allProducts[$image.image] = $temp;
+   echo $image.id;
    echo '<img src="data:image/'.$image.img_type.','.base64_encode($image.image).'" />';
 }
 //On transmets les variables à Smarty
