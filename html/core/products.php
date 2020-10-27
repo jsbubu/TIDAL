7d98a2c3ae7ce4cda0ac3a4ecf662074f6ecf8ed
lorismrcr <loris.mercier@cpe.fr>
2020-10-27 18:58:44 +0100

    Ajour de la fonction editIneById

--- html/core/products.php
+++ html/core/products.php
@@ -8,32 +8,50 @@ class products
         return ($res);
     } 
 
-    //Récuperer un produit par l'id
-    public function getOneById($id_prod){
-        //Récupérer un produit en particulier via l'id
+    //Modifie un produit en particulier via l'id
+    public function editOneById($id,$nom,$description,$filtre,$price,$base64,$img_type,$image_desc){
+        try {
+            global $bdd;
+            $bdd->query("UPDATE Products
+            SET name = '$nom',
+                description = '$description',
+                filtre = '$filtre',
+                price = '$price',
+                image = '$base64',
+                img_type = '$img_type',
+                img_description = '$image_desc',
+            WHERE id = '$id'");
+        }
+        catch (Exception $e) {
+            echo 'Erreur : ' . $e->getMessage() . '<br />';
+            echo 'N° : ' . $e->getCode();
+        }
+
     } 
 
     //Récuperer un produit par le nom
     public function getneByName($name_prod){
-       try {
-        global $bdd;
-        $result = $bdd->query("SELECT * FROM Products WHERE name='$name_prod'")->fetchAll(PDO::FETCH_ASSOC);
-        return ($result);
-            } catch (Exception $e) {
-        echo 'Erreur : ' . $e->getMessage() . '<br />';
-        echo 'N° : ' . $e->getCode();
-    }
+        try {
+            global $bdd;
+            $result = $bdd->query("SELECT * FROM Products WHERE name='$name_prod'")->fetchAll(PDO::FETCH_ASSOC);
+            return ($result);
+        } 
+        catch (Exception $e) {
+            echo 'Erreur : ' . $e->getMessage() . '<br />';
+            echo 'N° : ' . $e->getCode();
+        }
     } 
 
     //Ajout d'un produit dans la base de donnée
     public function addOneProduct($nom,$description,$filtre,$price,$base64,$img_type,$image_desc){
-    try {
-        global $bdd;
-        $bdd->query("INSERT INTO Products (name,description,filtre,price,image,img_type,img_description) VALUES ('$nom','$description','$filtre','$price','$base64','$img_type','$image_desc')");
-    } catch (Exception $e) {
-        echo 'Erreur : ' . $e->getMessage() . '<br />';
-        echo 'N° : ' . $e->getCode();
-    }
+        try {
+            global $bdd;
+            $bdd->query("INSERT INTO Products (name,description,filtre,price,image,img_type,img_description) VALUES ('$nom','$description','$filtre','$price','$base64','$img_type','$image_desc')");
+        } 
+        catch (Exception $e) {
+            echo 'Erreur : ' . $e->getMessage() . '<br />';
+            echo 'N° : ' . $e->getCode();
+        }
     }
 }
 ?>
