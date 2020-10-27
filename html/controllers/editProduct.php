0fd09c6aee9eaf234884e8ebbe7b3acadd7c0b60
lorismrcr <61010042+lorismrcr@users.noreply.github.com>
2020-10-27 13:43:42 +0100

    Update editProduct.php

--- html/controllers/editProduct.php
+++ html/controllers/editProduct.php
@@ -6,7 +6,6 @@ if(!empty($_POST['ProductName'])){
   $ProductName = htmlspecialchars($_POST['ProductName']);
   $thisProduct = products::getneByName($ProductName);
   $smarty->assign('this', $thisProduct);
-  echo "test1";
 }
 //On transmets les variables à Smarty
 $smarty->assign('products', $allProducts);
