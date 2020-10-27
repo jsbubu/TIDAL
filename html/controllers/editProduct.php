d67f2e9192e07df6f97dcf49a25c05e14b12a42d
lorismrcr <61010042+lorismrcr@users.noreply.github.com>
2020-10-27 13:32:18 +0100

    Update editProduct.php

--- html/controllers/editProduct.php
+++ html/controllers/editProduct.php
@@ -2,7 +2,7 @@
 //On appelle la fonction getAll()
 $allProducts = products::getAll();
 //On appelle la fonction ()
-if(isset($_POST['ProductName'])){
+if(empty($_POST['ProductName'])){
   $ProductName = htmlspecialchars($_POST['ProductName']);
   $thisProduct = products::getneByName($ProductName);
   $smarty->assign('this', $thisProduct);
