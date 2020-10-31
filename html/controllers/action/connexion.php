ccbd1510182626f35c3f72d9d105d91272323165
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-10-31 17:49:43 +0100

    Update connexion.php

--- html/controllers/action/connexion.php
+++ html/controllers/action/connexion.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = " ";
 $dbname= "tragront";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
