cee14252e4ac8546018515a13dd9249c329e8018
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-10-31 14:01:50 +0100

    Add files via upload

---
+++ html/controllers/action/connexion.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "ahmed";
+$password = "Je$uisendiete26/09/2020";
+$dbname= "tragront";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+
+$first = $_POST['prenom'];
+$last = $_POST['nom'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$adress = $_POST['adresse'];
+$postcode = $_POST['zip'];
+$ville = $_POST['city'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO users (first, last, email, phone, adress, zip, city, login, password) 
+VALUES ( '$first', '$last', '$email', '$phone', '$adress', '$postcode', '$ville', '$login', '$password');";
+
+mysqli_query($conn, $sql);
+
+
+
+
+?>  
