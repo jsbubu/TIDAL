f3ef309bb4d914d76a93e6249bee88f15e86a40d
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-11-06 23:08:35 +0100

    Update connexion.php

--- html/controllers/action/connexion.php
+++ html/controllers/action/connexion.php
@@ -12,8 +12,8 @@ echo "<!DOCTYPE html>
 
 <head>
     <meta charset='UTF-8'>
-    <link rel='stylesheet' href='../projet/style.css' />
-    <link rel='stylesheet' href='../projet/style2.css' />
+    <link rel='stylesheet' href='TIDAL/html/web/css/style.css' />
+    <link rel='stylesheet' href='TIDAL/html/web/css/style.css' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <title>Création de compte</title>
