653d73ff7c2e6e4b7e4b76ae055055aa02cc1aca
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-11-06 23:56:44 +0100

    Update connexion.php

--- html/controllers/action/connexion.php
+++ html/controllers/action/connexion.php
@@ -7,21 +7,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "<!DOCTYPE html>
-<html lang='fr'>
-
-<head>
-    <meta charset='UTF-8'>
-    <link rel='stylesheet' href='../../web/css/style.css' />
-    <link rel='stylesheet' href='../../web/css/style2.css' />
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-    <title>Création de compte</title>
-</head>
-
-<body>
-<h1>Välkommen !</h1> <p>Votre inscription s'est déroulé avec succés. Si vous souahitez vous connecter cliquer ici</p><p>Sinon cliquez ici pour retourner à l'accueil du site</p>";
-
+header('Location: welcome.tpl'); 
 
 $firstname = $_POST['prenom'];
 $familyname = $_POST['nom'];
