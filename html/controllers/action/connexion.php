473a5179b087467e3edaa9d3c6f9cb6eb963cd0d
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-11-06 23:31:42 +0100

    Update connexion.php

--- html/controllers/action/connexion.php
+++ html/controllers/action/connexion.php
@@ -12,8 +12,8 @@ echo "<!DOCTYPE html>
 
 <head>
     <meta charset='UTF-8'>
-    <link rel='stylesheet' href='../../../css/style.css' />
-    <link rel='stylesheet' href='../../../css/style2.css' />
+    <link rel='stylesheet' href='../../web/css/style.css' />
+    <link rel='stylesheet' href='../../web/css/style2.css' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <title>Création de compte</title>
