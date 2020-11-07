f6d8c2f156ddc3556a1098fe69d8a69e72de1fea
lorismrcr <loris.mercier@cpe.fr>
2020-11-07 20:32:38 +0100

    test

--- html/controllers/basket.php
+++ html/controllers/basket.php
@@ -11,10 +11,6 @@ if(isset($_POST['FindBasket'])){
     }
   $Idproduct = implode (',', $TabIdProd);
   $Userproduct = products::getneById($Idproduct);
-  $Key=array_search($produits.id,$basket)
-    if($Key){
-        $quantity=$basket[$Key][quantity];
-  }
   $smarty->assign('products', $Userproduct);
   $smarty->assign('basket', $BasketUser);
 }
