e5ef681b4cc1873e4b88aa2b382928e15fba7eb9
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-11-11 23:50:27 +0100

    Update function.php

--- html/controllers/function.php
+++ html/controllers/function.php
@@ -38,14 +38,14 @@ mysql_stmt_close($stmt);
 
 function createUser($conn, $first, $last, $adress, $login, $hashedpwd) {
 
-    $sql = "INSERT INTO Customers (firstname, lastname, adress, username, password) VALUES ('$first', '$last', '$adress', '$login', '$hashedpwd');";
+    $sql = "INSERT INTO Customers (firstname, lastname, adress, username, password) VALUES (?, ?, ?, ?, ?);";
     $stmt = mysqli_stmt_init($conn); 
     if (!mysqli_stmt_prepare($stmt, $sql)) {
     header("location: ../tpl/pages/signup.tpl?error=stmtfailed");
     exit();  
     }
 $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
-mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $adress, $login, $password, $has);
+mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $adress, $login, $password);
 mysqli_stmt_execute($stmt);
 mysql_stmt_close($stmt);
 header("location: ../tpl/pages/signup.tpl?error=none");
