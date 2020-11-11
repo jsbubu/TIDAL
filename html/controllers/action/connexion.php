3f6cab6930027a4e4c49a29455ed3c319ae51cb2
ahmedaall <61010064+ahmedaall@users.noreply.github.com>
2020-11-11 22:01:37 +0100

    Update connexion.php

--- html/controllers/action/connexion.php
+++ html/controllers/action/connexion.php
@@ -6,20 +6,39 @@ $dbname= "tragront";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
+}      
+
+require_once 'function.php';
+if (isset($_POST['submit'])) {
+  $first = $_POST['prenom'];
+  $last = $_POST['nom'];
+  $adress = $_POST['adresse'];
+  $login = $_POST['login'];
+  $password = $_POST['password'];
+  $hashedpwd = password_hash($password, PASSWORD_DEFAULT).
+  $passwordRepeat = $_POST['password2'];
+
+  if(pwdMatch($password, $passwordRepeat) !== false) {
+   header ("location: ./signup.php?error=pwdnomatch"); 
+  }
+
+  if(uidExist($conn, $login) !== false){
+    header ("location: ./signup.php?error=UidAlreadyExist"); 
+  }
+  
+  $sql = "INSERT INTO users (first, last, adress, login, password) 
+  VALUES ( '$first', '$last', '$adress', '$login', '$hashedpwd');";
+
+  mysqli_query($conn, $sql);
+  echo "<h1>Välkommen !</h1> 
+  <p>Vous êtes bien inscrit. Si vous souahitez vous connecter cliquer <a href=../projet/signup.html>ici</a></p>
+  <p>Sinon cliquez ici pour retourner à la racine du site</p> ";
+
+} else {
+
+  header('Location: signup.html');      
+
 }
-header('Location: ../../tpl/pages/welcome.tpl'); 
-
-$firstname = $_POST['prenom'];
-$familyname = $_POST['nom'];
-$adress = $_POST['adresse'];
-$username = $_POST['login'];
-$password = $_POST['password'];
-
-$sql = "INSERT INTO Customers (firstname, familyname, address, username, password) 
-VALUES ( '$firstname', '$familyname','$adress', '$username', '$password');";
-
-mysqli_query($conn, $sql);
-
 
 
 
